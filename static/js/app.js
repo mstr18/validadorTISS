87351dd0f9c7c064da76bb30ee2a48cb7ac1f003
Iago Chiapetta <iagochiapetta@gmail.com>
2024-04-10 15:45:17 -0300

    arquivos estáticos

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var xmlString = event.target.result;
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+        var ansPadrao = xmlDoc.querySelector('//ans:Padrao'); // Altere para o seletor correto se necessário
+        if (ansPadrao) {
+            var tissVersion = ansPadrao.textContent.replace('.', '_');
+            document.getElementById('versionSelect').value = tissVersion;
+        } else {
+            alert('Número de versão do TISS não encontrado no arquivo XML.');
+        }
+    };
+    reader.readAsText(file);
+});
